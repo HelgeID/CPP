35a303b5b54317f3f760daae2d75ab0318840959
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-08-14 09:39:20 +0300

    Add files via upload

---
+++ паралельне програмування/list1.0.0 паралельне програмування, емуляція довгої роботи головного потока.cpp
@@ -0,0 +1,28 @@
+﻿//паралельне програмування, емуляція довгої роботи головного потока
+#include <iostream>
+#include <thread> //для роботи з потоками
+#include <chrono> //для роботи з часом
+
+//ф-я виводу в консоль
+void Print(char* text)
+{
+	std::cerr << text << std::endl;
+	return;
+}
+
+int main()
+{
+	//start
+	Print("START main thread");
+	std::cerr << "ID thread: " << std::this_thread::get_id() << std::endl; //ідентифікатор потоку
+
+	//pause
+	Print("PAUSE main thread: 5s...");
+	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
+	//stop
+	Print("STOP main thread");
+
+	system("pause");
+	return 0;
+}
