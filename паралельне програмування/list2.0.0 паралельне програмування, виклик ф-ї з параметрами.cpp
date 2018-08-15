2927a3378677045a436ec8880ed155ffc617ed00
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-08-15 20:03:49 +0300

    Add files via upload

---
+++ паралельне програмування/list2.0.0 паралельне програмування, виклик ф-ї з параметрами.cpp
@@ -0,0 +1,20 @@
+﻿//паралельне програмування, виклик ф-ї з параметрами
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void MyFunc(int a, int b)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(250));
+	int res = a + b;
+	std::cerr << a << "+" << b << "=" << res << std::endl;
+	return;
+}
+
+int main()
+{
+	std::thread t(MyFunc, 3, 2); //виклик ф-ї MyFunc з параметрами в другому потоці
+	t.join();
+	system("pause");
+	return 0;
+}
