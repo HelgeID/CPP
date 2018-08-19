a6bbe6e35fb4dbebd3d3d9906ae9a4d38129dd68
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-08-19 10:36:49 +0300

    Add files via upload

---
+++ паралельне програмування/list3.0.0 паралельне програмування, одержання оброблених даних з другого потоку (передача по посиланню).cpp
@@ -0,0 +1,21 @@
+﻿//паралельне програмування, одержання оброблених даних з другого потоку (передача по посиланню)
+#include <iostream>
+#include <thread>
+
+void MyFunc(int &number)
+{
+	number *= 10;
+	return;
+}
+
+int main()
+{
+	int number = 5;
+	std::thread t(MyFunc, std::ref(number));
+	t.join();
+
+	std::cerr << number << std::endl; //50
+
+	system("pause");
+	return 0;
+}
