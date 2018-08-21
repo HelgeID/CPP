1b47c556427c4bee0e08eb1fa6d520584c0cc5a9
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-08-21 14:31:39 +0300

    Add files via upload

---
+++ паралельне програмування/list4.0.0 паралельне програмування, виклик методів класу в другому потоці.cpp
@@ -0,0 +1,52 @@
+﻿//паралельне програмування, виклик методів класу в другому потоці
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+class MyClass
+{
+public:
+	void MyMethod()
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::cerr << "start second" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		std::cerr << "stop second" << std::endl;
+		return;
+	}
+
+	int MyFunc(int value)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::cerr << "start second" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+		std::cerr << "stop second" << std::endl;
+		return value*value;
+	}
+};
+
+int main()
+{
+	int res;
+	MyClass obj;
+
+	//п-д 1:
+	std::thread t([&]()
+	{
+		res = obj.MyFunc(10);
+	});
+
+	//п-д 2:
+	//std::thread t(obj.MyMethod); //error
+	//std::thread t(&MyClass::MyMethod, obj); //ok
+	//std::thread t(&MyClass::MyFunc, obj, 10); //ok
+
+	for (size_t i(0); i < 10; i++) {
+		std::cerr << "main" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(750));
+	}
+	t.join();
+
+	system("pause");
+	return 0;
+}
