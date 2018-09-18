f9a5f0707a105f18f7a9f235c5997ad6fe9f4d1a
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-09-18 10:40:10 +0300

    Add files via upload

---
+++ паралельне програмування/list5.0.6 паралельне програмування, очікування за допомогою умовних змінних (приклад).cpp
@@ -0,0 +1,47 @@
+﻿//паралельне програмування, очікування за допомогою умовних змінних (приклад)
+
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <condition_variable>
+
+bool is_ready(false);
+std::mutex mtx;
+std::condition_variable cv;
+
+void DoWork()
+{
+	for (int index(8); index >=0; --index) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(400));
+		std::cerr << index << " ";
+	}
+	std::cerr << std::endl;
+	return;
+}
+
+void MyFun()
+{
+	DoWork();
+	std::unique_lock<std::mutex> ul(mtx);
+	is_ready = true;
+	cv.notify_one();
+	return;
+}
+
+int main()
+{
+	std::thread t(MyFun);
+	std::unique_lock<std::mutex> ul(mtx);
+	while (!is_ready)
+	{
+		cv.wait(ul);
+		if (!is_ready)
+			std::cerr << "Spurious wake up!\n";//Помилкове пробудження!
+	}
+	t.join();
+
+	system("pause");
+	return 0;
+}
