a4cc44fc1a153c8863ab342ae685f9673cf11990
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-09-23 06:26:45 +0300

    Add files via upload

---
+++ new C++ STL/list001, структуровані привязки, pair(пара).cpp
@@ -0,0 +1,17 @@
+﻿//new C++, структуровані привязки, pair(пара)
+#include <iostream>
+std::pair<int, int> Div(int dividend, int divisor);
+
+int main()
+{
+	const auto res(Div(5, 3));
+	std::cerr << res.first << " " << res.second << std::endl; //1 2
+
+	system("pause");
+	return 0;
+}
+
+std::pair<int, int> Div(int dividend, int divisor)
+{
+	return std::pair<int, int>(dividend / divisor, dividend % divisor);
+}
