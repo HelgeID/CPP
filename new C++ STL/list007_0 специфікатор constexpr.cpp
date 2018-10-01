c08d31db46f1a474cd4d3140f066a8211734da29
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-10-01 21:05:58 +0300

    Add files via upload

---
+++ new C++ STL/list007_0 специфікатор constexpr.cpp
@@ -0,0 +1,23 @@
+﻿//new C++, специфікатор constexpr
+#include <iostream>
+
+//work
+struct MyStruct
+{
+	//про змінну value відомо на етапі компіляції
+	constexpr static double value = 10.0;
+};
+
+//not work
+//struct MyStruct
+//{
+//	const static double value = 10.0; //не може бути проініціалізована (не компілюється)
+//};
+
+int main()
+{
+	MyStruct my;
+	std::cerr << my.value << std::endl; //10
+	system("pause");
+	return 0;
+}
