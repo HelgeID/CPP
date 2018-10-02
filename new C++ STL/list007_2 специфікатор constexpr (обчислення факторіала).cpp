a89545d6ac995a166126cff1714dae6a3cec0374
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-10-02 06:01:57 +0300

    Add files via upload

---
+++ new C++ STL/list007_2 специфікатор constexpr (обчислення факторіала).cpp
@@ -0,0 +1,25 @@
+﻿//new C++, специфікатор constexpr (обчислення факторіала)
+#include <iostream>
+
+// c++11
+constexpr int factorial1(const int value)
+{
+	return (value == 0 ? 1 : value * factorial1(value - 1));
+}
+
+// c++14
+constexpr int factorial2(const int value)
+{
+	auto result = 1;
+	for (int i = 1; i <= value; i++)
+		result *= i;
+	return result;
+}
+
+int main()
+{
+	//обчислення виконуються в compile-time, а не в run-time
+	std::cerr << factorial1(5) << std::endl; //120
+	std::cerr << factorial2(5) << std::endl; //120
+	return 0;
+}
