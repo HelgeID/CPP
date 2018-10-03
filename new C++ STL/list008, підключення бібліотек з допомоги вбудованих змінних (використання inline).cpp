be6a26c62043748919ceb9288e0f1105225e654f
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-10-04 00:44:00 +0300

    Add files via upload

---
+++ new C++ STL/list008, підключення бібліотек з допомоги вбудованих змінних (використання inline).cpp
@@ -0,0 +1,58 @@
+﻿//new C++, підключення бібліотек з допомоги вбудованих змінних (використання inline)
+//---------------------------------------------------------
+File: mylib.h
+//---------------------------------------------------------
+#include<string>
+#pragma once
+class MyClass
+{
+public:
+    static inline std::string str{"HI!"};
+    
+    static std::string& str_fun()
+    {
+        return str;
+    }
+    
+    static void set(const std::string);
+};
+
+MyClass obj; //об'єкт можемо використовувати скрізь
+
+//---------------------------------------------------------
+File: mylib.cpp
+//---------------------------------------------------------
+#include "mylib.h"
+
+static void MyClass::set(std::string _str)
+{
+    str = _str;
+}
+
+//---------------------------------------------------------
+File: mylib2.h
+//---------------------------------------------------------
+#include "mylib.h"
+
+class SET { public: SET(const std::string str) { obj.str = str; } };
+
+//---------------------------------------------------------
+File: main.cpp
+//---------------------------------------------------------
+#include <iostream>
+#include "mylib.h"
+#include "mylib2.h"
+
+int main()
+{
+    /////////////////////////////////////////
+    std::cout << obj.str << std::endl;
+    /////////////////////////////////////////
+    const std::string str = "Hello, World!";
+    obj.str_fun() = str;
+    std::cout << obj.str << std::endl;
+    /////////////////////////////////////////
+    SET set("ABC");
+    std::cout << obj.str << std::endl;
+    return 0;
+}
