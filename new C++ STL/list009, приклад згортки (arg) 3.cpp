6ac97ed6f47e30204f9ba649803944cc7e0affe6
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-10-08 06:11:44 +0300

    Add files via upload

---
+++ new C++ STL/list009, приклад згортки (arg) 3.cpp
@@ -0,0 +1,27 @@
+﻿//new C++, приклад згортки (arg) 3 (перевірка попадання всіх параметрів в заданий діапазон)
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+template <typename T, typename ... Ts>
+bool IS(T min, T max, Ts ...ts)
+{
+    return ((min <= ts && ts <= max) && ...);
+}
+
+int main()
+{
+    //std::cerr << IS(10, 20, 1, 15, 30) << std::endl; //false
+    //std::cerr << IS(10, 20, 11, 12, 13) << std::endl; //true
+    //std::cerr << IS(5.0, 5.5, 5.1, 5.2, 5.3) << std::endl; //true
+    
+    //робота з стрінгом
+    std::string aaa {"aaa"};
+    std::string bcd {"bcd"};
+    std::string def {"def"};
+    std::string zzz {"zzz"};
+
+    std::cerr << IS(aaa, zzz, bcd, def) << std::endl; //true
+    std::cerr << IS(aaa, def, bcd, zzz) << std::endl; //false
+    return 0;
+}
