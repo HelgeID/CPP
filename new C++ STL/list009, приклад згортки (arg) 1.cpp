cdba69fa1fb952dda3d6a40de8aa793eb1872996
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-10-08 06:10:04 +0300

    Add files via upload

---
+++ new C++ STL/list009, приклад згортки (arg) 1.cpp
@@ -0,0 +1,21 @@
+﻿//new C++, приклад згортки (arg) 1 (співвіднесення діапазонів і окремих елементів)
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template <typename R, typename ... Ts>
+auto Count(const R& range, Ts ... ts)
+{
+    return (std::count(std::begin(range), std::end(range), ts) + ...);
+}
+
+int main()
+{
+    std::vector<int> vec {10, 20, 30, 40, 50}; //також працюватиме для std::list, std::array, std::set
+    std::cerr << Count(vec, 0, 20) << std::endl; //1
+    std::cerr << Count(vec, 10, 30) << std::endl; //2
+    std::cerr << Count(vec, 0) << std::endl; //0
+    std::cerr << Count("ABCD", 'A', 'b', 'c', 'D') << std::endl; //2
+    std::cerr << Count("ABCD", 'a', 'B', 'C', 'd') << std::endl; //2
+    return 0;
+}
