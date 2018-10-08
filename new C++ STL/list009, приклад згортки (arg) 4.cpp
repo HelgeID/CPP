752f0608e6bcb43af6fbc2a5d812d98af9608c46
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-10-08 06:12:12 +0300

    Add files via upload

---
+++ new C++ STL/list009, приклад згортки (arg) 4.cpp
@@ -0,0 +1,19 @@
+﻿//new C++, приклад згортки (arg) 4 (відправлення декількох елементів у вектор)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T, typename ... Ts>
+void InsertAll(std::vector<T> &vec, Ts ... ts)
+{
+    (vec.push_back(ts), ...);
+}
+
+int main()
+{
+    std::vector<int> vec{10, 20, 30};
+    InsertAll(vec, 40, 50, 60);
+    for (int i(0); i<vec.size(); ++i)
+        std::cerr << vec.at(i) << std::endl;
+    return 0;
+}
