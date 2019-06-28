84e57c52150b0f6eeb0648b9b8c96b2ffb3999c8
HelgeID <34999595+HelgeID@users.noreply.github.com>
2019-06-28 16:45:47 +0300

    Add files via upload

---
+++ паралельне програмування/list6.0.2 паралельне програмування (стан потоку).cpp
@@ -0,0 +1,25 @@
+﻿//std::thread + std::atomic
+#include <iostream>
+
+#include <thread>
+#include <atomic>
+#include <chrono>
+
+int main()
+{
+	std::atomic<bool> done(false); //атомарний флаг
+
+	//запускаємо завдання в окремому потоці
+	std::thread t([&done] {
+		std::cout << "Thread still running" << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+		done = true;
+	});
+	t.join();
+
+	if (done)
+		std::cout << "Thread finished" << std::endl;
+
+	system("pause");
+	return 0;
+}
